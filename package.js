cbd4abc7fd5b662b0f1f83cfb487e00752d369af
Mitar <mitar.git@tnode.com>
2019-09-19 21:20:26 -0700

    Version bump.

--- package.js
+++ package.js
@@ -1,16 +1,16 @@
 Package.describe({
   name: 'peerlibrary:data-lookup',
   summary: "Reactively lookup a field in the object",
-  version: '0.2.1',
+  version: '0.3.0',
   git: 'https://github.com/peerlibrary/meteor-data-lookup.git'
 });
 
 Package.onUse(function (api) {
-  api.versionsFrom('METEOR@1.4.4.5');
+  api.versionsFrom('METEOR@1.8.1');
 
   // Core dependencies.
   api.use([
-    'coffeescript@2.0.3_3',
+    'coffeescript@2.4.1',
     'ecmascript',
     'underscore',
     'tracker'
@@ -18,7 +18,7 @@ Package.onUse(function (api) {
 
   // 3rd party dependencies.
   api.use([
-    'peerlibrary:computed-field@0.8.0'
+    'peerlibrary:computed-field@0.10.0'
   ]);
 
   api.export('DataLookup');
@@ -27,11 +27,11 @@ Package.onUse(function (api) {
 });
 
 Package.onTest(function (api) {
-  api.versionsFrom('METEOR@1.4.4.5');
+  api.versionsFrom('METEOR@1.8.1');
 
   // Core dependencies.
   api.use([
-    'coffeescript@2.0.3_3',
+    'coffeescript@2.4.1',
     'ecmascript',
     'random',
     'underscore',
@@ -45,8 +45,8 @@ Package.onTest(function (api) {
 
   // 3rd party dependencies.
   api.use([
-    'peerlibrary:classy-test@0.3.0',
-    'peerlibrary:server-autorun@0.7.1'
+    'peerlibrary:classy-test@0.4.0',
+    'peerlibrary:server-autorun@0.8.0'
   ]);
 
   api.addFiles([
